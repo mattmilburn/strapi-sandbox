f8a7c7e544b9321b88ff72cf29605a1b57a6ed9a
Matt Milburn <me@mattmilburn.com>
2022-12-15 14:33:29 -0600

    Add upgrade bash scripts

---
+++ upgrade-strapi-design.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+yarn upgrade @strapi/design-system@$1 @strapi/icons@$1
---
+++ upgrade-strapi.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+yarn upgrade @strapi/helper-plugin@$1 @strapi/plugin-i18n@$1 @strapi/plugin-users-permissions@$1 @strapi/strapi@$1 @strapi/utils@$1
